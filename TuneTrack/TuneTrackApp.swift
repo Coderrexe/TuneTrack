59f429e4f6aec3f01a7c0960375da7a1de612155
Simba Shi <shizimo@gmail.com>
2024-09-11 15:27:43 +0100

    Initial Commit

---
+++ TuneTrack/TuneTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneTrackApp.swift
+//  TuneTrack
+//
+//  Created by Shi, Simba (Coll) on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TuneTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
