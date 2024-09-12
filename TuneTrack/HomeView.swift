54fbe965d1af4d707e48f885bffa73511e7e7960
Simba Shi <shizimo@gmail.com>
2024-09-12 11:19:33 +0100

    Initial commit.

---
+++ TuneTrack/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TuneTrack
+//
+//  Created by Shi, Simba (Coll) on 12/09/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
